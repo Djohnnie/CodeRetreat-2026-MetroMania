1333669db8902a6022004321cc0b4dd63b108828
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2026-04-02 10:41:16 +0200

    Added support for level data import and export

---
+++ src/MetroMania.Web/wwwroot/app.js
@@ -0,0 +1,16 @@
+function downloadFile(filename, content, mimeType) {
+    const blob = new Blob([content], { type: mimeType });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+function clickElementById(id) {
+    const el = document.getElementById(id);
+    if (el) el.click();
+}
